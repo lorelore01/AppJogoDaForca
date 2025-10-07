18bf6a94ae2f5dcc34d0483383828a9b1a241cb1
Lorenzo <lorenzodonato75@Gmail.com>
2025-10-06 22:30:54 -0300

    chore: initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppJogoDaForca;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppJogoDaForca;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
